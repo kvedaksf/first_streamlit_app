5dbcc732eb1677a9531c34b6391f825c8f28c408
kunal vedak <114462829+kvedaksf@users.noreply.github.com>
2022-10-30 18:15:20 +0530

    added userinput step

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruit_choice = streamlit.text_input("Which fruit information would you like?",'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 #normalize the output
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
