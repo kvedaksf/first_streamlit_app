0230068d73d1c91a15df84e727edff1dfd1f57f6
kunal vedak <114462829+kvedaksf@users.noreply.github.com>
2022-11-01 18:30:23 +0530

    trouble shoot for back_from_function

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ add_my_fruit= streamlit.text_input("Which fruit would you like to add?")
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function=insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_button)
+  streamlit.text(back_from_function)
 #streamlit.write('The user entered', add_my_fruit)
 streamlit.stop()
 
