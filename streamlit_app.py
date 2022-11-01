12e7ab6c3562ef999453d7d9a58ad8f5d022dbdf
kunal vedak <114462829+kvedaksf@users.noreply.github.com>
2022-11-01 18:47:58 +0530

    fixing the insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if streamlit.button('Get fruit list'):
 #allow end user to add fruit
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("Insert into fruit_load_list values('from streamlit')")
+    my_cur.execute("Insert into fruit_load_list values(new_fruit)")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit= streamlit.text_input("Which fruit would you like to add?")
